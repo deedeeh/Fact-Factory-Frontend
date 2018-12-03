657c5e236dfc96b71eb0dad2609a3fac2dccfc57
internap627 <you@example.com>
2018-12-03 14:31:46 +0000

    like heart

--- src/components/FactCard.js
+++ src/components/FactCard.js
@@ -1,9 +1,25 @@
 import React from 'react'
-import { Card } from 'semantic-ui-react'
+import { Card, Icon } from 'semantic-ui-react'
+
+
 
 const FactCard = ({ fact }) => {
+    const extra = (
+        <a>
+          <Icon name='heart outline' />
+          {fact.likes}
+        </a>
+      )
+    
     return (
-        <Card className='fact-card' content={fact.content}   />
+        
+        <Card 
+            header='Fact'
+            description={fact.content}
+            extra={extra}
+        />
+      
+       
     )
 }
 
